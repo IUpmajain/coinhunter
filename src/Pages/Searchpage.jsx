8ba88b1800c3d5e2e0276447c1b5fd631bef9078
unknown <upmajain2305@gmail.com>
2023-12-11 13:03:42 +0530

    updats

--- src/Pages/Searchpage.jsx
+++ src/Pages/Searchpage.jsx
@@ -1,13 +1,39 @@
-import React from 'react'
-import Search  from "../component/Search";
+import React, { useContext, useState } from 'react'
+import CoinContext from '../context/CoinContext';
+import { SearchCoin } from '../context/CoinAction';
 import CarcContainer from '../component/CarcContainer';
 
 
 
 const Searchpage = () => {
+
+  const { dispatch } = useContext(CoinContext);
+
+  const [text, setText] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    const data = await SearchCoin(text);
+
+    dispatch({
+      type: "SEARCH_COINS",
+      payload: data,
+    });
+
+    setText("");
+  }
+
   return (
     <div>
-      <Search/>
+
+
+  
+      <form className="d-flex m-2" role="search" onSubmit={handleSubmit}>
+        <input className="form-control me-2 " type="search" placeholder="Search any coin at here" aria-label="Search" onChange={(e) => setText(e.target.value)}
+        value={text}/>
+        <button className="btn btn-success w-25" type="submit">Search</button>
+      </form>
+  
       <CarcContainer/>
     </div>
   )
--- src/component/Search.jsx
+++ src/component/Search.jsx
@@ -1,35 +1,39 @@
-import React, { useContext, useState } from 'react'
-import CoinContext from '../context/CoinContext';
-import { SearchCoin } from '../context/CoinAction';
+// import React, { useContext, useState } from 'react'
+// import CoinContext from '../context/CoinContext';
+// import { SearchCoin } from '../context/CoinAction';
+// import CarcContainer from './CarcContainer';
 
-const Search = () => {
+// const Search = () => {
 
-  const { dispatch } = useContext(CoinContext);
+//   const { dispatch } = useContext(CoinContext);
 
-  const [text, setText] = useState("");
+//   const [text, setText] = useState("");
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    const data = await SearchCoin(text);
+//   const handleSubmit = async (e) => {
+//     e.preventDefault();
+//     const data = await SearchCoin(text);
 
-    dispatch({
-      type: "SEARCH_COINS",
-      payload: data,
-    });
+//     dispatch({
+//       type: "SEARCH_COINS",
+//       payload: data,
+//     });
 
-    setText("");
-  };
+//     setText("");
+//   };
 
   
 
-  return (
+//   return (
 
-      <form className="d-flex m-2" role="search" onSubmit={handleSubmit}>
-        <input className="form-control me-2 " type="search" placeholder="Search any coin at here" aria-label="Search" onChange={(e) => setText(e.target.value)}
-        value={text}/>
-        <button className="btn btn-success w-25" type="submit">Search</button>
-      </form>
-  )
-}
+//       <>
+//       <form className="d-flex m-2" role="search" onSubmit={handleSubmit}>
+//         <input className="form-control me-2 " type="search" placeholder="Search any coin at here" aria-label="Search" onChange={(e) => setText(e.target.value)}
+//         value={text}/>
+//         <button className="btn btn-success w-25" type="submit">Search</button>
+//       </form>
+//       <CarcContainer/>
+//       </>
+//   )
+// }
 
-export default Search;
+// export default Search;
